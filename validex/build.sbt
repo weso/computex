968e7b6173dde47744b843cb6daa594d1f00fd5e
labra <jelabra@gmail.com>
2013-06-15 11:41:04 +0200

    Simply check files in validation dir

--- validex/build.sbt
+++ validex/build.sbt
@@ -4,3 +4,9 @@ version := "1.0"
 
 scalaVersion :="2.9.2"
 
+libraryDependencies ++= Seq(
+  "junit" % "junit" % "4.8" % "test",
+  "commons-configuration" % "commons-configuration" % "1.7",
+  "org.apache.jena" % "jena-arq" % "2.9.3" 
+)
+
--- validex/src/main/scala/es/weso/validex/validex.scala
+++ validex/src/main/scala/es/weso/validex/validex.scala
@@ -1,5 +1,41 @@
 package es.weso.validex
 
+import scala.collection.JavaConversions._
+import java.io._
+import com.hp.hpl.jena.rdf.model.ModelFactory
+import com.hp.hpl.jena.query._
+import com.hp.hpl.jena.query.Query._
+import com.hp.hpl.jena.ontology.OntModelSpec._
+
+
 object Application extends App {
-   println("Validex: Validating index data")
+  
+  val validationDirName = "../ontology/validation"
+
+    try {
+     println("Validex: Validating index data")
+     printCWD()
+     readValidationFiles();
+    } catch {
+      case ex : Exception => println("Exception: " + ex)
+    }
+
+def printCWD() {
+  val cwd = new File(".")
+
+  println("CWD = " + cwd.getAbsolutePath())
+}
+   
+ def readValidationFiles() {
+  val validationDir = new File(validationDirName)
+
+  if (validationDir == null || validationDir.listFiles == null) 
+     throw new IOException("Validation directory: " + validationDirName + " not accessible")
+  else {
+    for(file <- validationDir.listFiles if file.getName endsWith ".sparql"){
+    // process the file
+     println("Validation file: " + file)
+    }   
+   }
+ }  
 }
