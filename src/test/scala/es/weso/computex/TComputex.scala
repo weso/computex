ae2960b97884adf4d53abd35dcb4e20e6e5bd524
labra <jelabra@gmail.com>
2013-06-19 09:28:33 +0200

    Added phase to pass integrity-tests from RDF Data Cube

--- src/test/scala/es/weso/computex/TComputex.scala
+++ src/test/scala/es/weso/computex/TComputex.scala
@@ -1,42 +1,77 @@
 package es.weso.computex
 
 import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
 import com.typesafe.config._
 import com.hp.hpl.jena.rdf.model.ModelFactory
 import com.hp.hpl.jena.query._
 import com.hp.hpl.jena.query.Query._
 import com.hp.hpl.jena.rdf.model._
+import com.hp.hpl.jena.util.FileManager;
+import scala.collection.JavaConversions._
 
-class ComputexSuite extends FunSuite with BeforeAndAfter {
+class ComputexSuite extends FunSuite with ShouldMatchers {
 
   val conf : Config = ConfigFactory.load()
 
   val validationDir = conf.getString("validationDir")
   val computationDir = conf.getString("computationDir")
+  val testDataDir = conf.getString("testDataDir")
   val ontologyURI  = conf.getString("ontologyURI")
   val indexDataURI_ok = conf.getString("indexDataURI_ok")
-  val indexDataURI_withErrors = conf.getString("indexDataURI_withErrors")
+  
   
   val cex = new Computex
-  val model_ok = cex.loadData(ontologyURI,indexDataURI_ok)
-  val model_withErrors = cex.loadData(ontologyURI,indexDataURI_withErrors)
-
-  test("just checking true...") {
-    assert(true,true)
-  }
   
+  val queryErrorMsg : String = 
+   """PREFIX cex: <http://purl.org/weso/ontology/computex#>
+   SELECT ?msg WHERE { 
+    ?e a cex:Error ; cex:msg ?msg . 
+   }"""
+
   test("No errors in good index data") {
-   val reportModel = cex.validate(model_ok,validationDir)  
-   assert(reportModel.size == 0)
+	  val model_ok = cex.loadData(ontologyURI,indexDataURI_ok)
+	  val errorModel = cex.validate(model_ok,validationDir) 
+	  errorModel.size should be(0)
   }
 
-  test("Errors in bad index data") {
-   val reportModel = cex.validate(model_withErrors,validationDir)  
+  test("Error if obs has no value") {
+   val model = loadExample("obs_noValue.ttl") 
+   val reportModel = cex.validate(model,validationDir)  
    assert(reportModel.size > 0)
+   assertMsgError(reportModel, "Observation does not have value")
   }
 
  test("Error if an observation has 2 values") {
-   val reportModel = cex.validate(model_withErrors,validationDir)
+   val model = loadExample("obs2Values.ttl") 
+   val reportModel = cex.validate(model,validationDir)  
+   assert(reportModel.size > 0)
+   assertMsgError(reportModel, "Observation has two different values")
   }
+
+   test("Error if an observation with Missing status has value") {
+   val model = loadExample("missingObsNoValues.ttl") 
+   val reportModel = cex.validate(model,validationDir)  
+   assert(reportModel.size > 0)
+   assertMsgError(reportModel, "Observation with Status obsStatus-M (Missing) should not have value")
+  }
+
+ def loadExample(name: String) : Model = {
+   val model = ModelFactory.createDefaultModel()
+   cex.loadTurtle(model,ontologyURI)
+   FileManager.get.readModel(model, testDataDir + name)
+   model
+ }
+ 
+ def assertMsgError(model : Model, msg: String) = {
+   val results = 
+		   QueryExecutionFactory.create(queryErrorMsg, model).execSelect;
+   val msgs = (for { r <- results } 
+                yield r.get("?msg").asLiteral.getString
+              ).toList
+    msgs should contain(msg)
+  }
+ 
+ 
 
 }
---
+++ src/test/scala/es/weso/computex/TCube.scala
@@ -0,0 +1,71 @@
+package es.weso.computex
+
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
+import com.typesafe.config._
+import com.hp.hpl.jena.rdf.model.ModelFactory
+import com.hp.hpl.jena.query._
+import com.hp.hpl.jena.query.Query._
+import com.hp.hpl.jena.rdf.model._
+import com.hp.hpl.jena.util.FileManager
+import scala.collection.JavaConversions._
+import java.io.IOException
+import java.io.File
+
+class CubeSuite extends FunSpec with ShouldMatchers {
+
+  val conf : Config = ConfigFactory.load()
+
+  val validationDir = conf.getString("validationDir")
+  val computationDir = conf.getString("computationDir")
+  val cubeDataDir = conf.getString("cubeDataDir")
+  val ontologyURI  = conf.getString("ontologyURI")
+  val indexDataURI_ok = conf.getString("indexDataURI_ok")
+  
+  
+  val cex = new Computex
+
+  val PREFIXES =
+"""
+PREFIX rdf:            <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs:           <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skos:           <http://www.w3.org/2004/02/skos/core#>
+PREFIX qb:             <http://purl.org/linked-data/cube#>
+PREFIX xsd:            <http://www.w3.org/2001/XMLSchema#>
+PREFIX owl:            <http://www.w3.org/2002/07/owl#>
+PREFIX eg:             <http://example.com/abbrv-cube/>
+"""
+    
+  describe("The example") {
+    describe("should pass all the RDF Data Cube integrity tests") {
+      val model_ok = cex.loadData(ontologyURI,indexDataURI_ok)
+
+      val dir = new File(cubeDataDir)
+	  if (dir == null || dir.listFiles == null) 
+		  throw new IOException("Directory: " + cubeDataDir + " not accessible")
+	  else {
+		  for (file <- dir.listFiles ;
+		       if file.getName startsWith "integrity" ;
+		       if file.getName endsWith ".sparql") {
+		    val name = file.getName.dropRight(7) // remove ".sparql" = 7 chars 
+		    val contents = PREFIXES + scala.io.Source.fromFile(file).mkString ;
+		    val query = QueryFactory.create(contents) 
+		    pass(name,query,model_ok)
+		  }
+	  }
+    }
+  }
+
+  def pass(name : String, query: Query, model: Model) = 
+    it("should pass integrity check: " + name) {
+     val qe = QueryExecutionFactory.create(query,model)
+     try {
+       val result = qe.execAsk();
+       result should equal(false);
+     } finally {
+       qe.close();
+     }
+	}
+
+
+}
