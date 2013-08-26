f4e03d06bdcb8e5ca069e541a3bc2702dfb7e5c1
César Luis Alvargonzález <info@cesarla.com>
2013-08-26 14:06:19 +0200

    Modified encoding detection in order to avoid #15

--- app/es/weso/computex/entities/CMessage.scala
+++ app/es/weso/computex/entities/CMessage.scala
@@ -1,11 +1,16 @@
 package es.weso.computex.entities
 
+import java.io.BufferedInputStream
 import java.io.ByteArrayInputStream
-import java.io.InputStream
-import scala.Array.canBuildFrom
-import org.apache.commons.io.IOUtils
-import es.weso.utils.JenaUtils
 import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import scala.Array.canBuildFrom
+import es.weso.utils.JenaUtils
+import java.nio.charset.CodingErrorAction
+import org.apache.commons.io.IOUtils
+import java.io.BufferedReader
 
 case class CMessage(val action: String) {
   var message: String = CMessage.MsgOK
@@ -19,15 +24,8 @@ case class CMessage(val action: String) {
   private var _contentIS: String = null
   private var _integrityQueries: Array[(String, IntegrityQuery)] = Array.empty
 
-  def contentIS_=(is: InputStream): Unit = _contentIS = IOUtils.toString(is, "UTF-8")
-
-  @throws(classOf[IOException])
-  def contentIS: InputStream = {
-    if (_contentIS == null) null
-    else new ByteArrayInputStream(_contentIS.getBytes("UTF-8"))
-  }
-
   def integrityQueries_=(iq: Array[(String, IntegrityQuery)]): Unit = _integrityQueries = iq
+
   def integrityQueries: Array[(String, IntegrityQuery)] = _integrityQueries.sortWith(_._1 < _._1)
 
   def size = _integrityQueries.map(_._2.size).foldLeft(0)(_ + _)
@@ -37,6 +35,29 @@ case class CMessage(val action: String) {
     case CMessage.MsgEmpty => CMessage.Idle
     case _ => CMessage.Invalid
   }
+
+  def contentIS_=(is: InputStream): Unit = _contentIS = {
+    val in = new BufferedInputStream(is);
+    val charsetDecoder = Charset.forName("UTF-8").newDecoder();
+    charsetDecoder.onMalformedInput(CodingErrorAction.REPLACE);
+    charsetDecoder.onUnmappableCharacter(CodingErrorAction.REPLACE);
+    val inputReader = new InputStreamReader(in, charsetDecoder);
+    val bufferedReader = new BufferedReader(inputReader);
+    val sb = new StringBuilder();
+    var line = bufferedReader.readLine();
+    while (line != null) {
+      sb.append(line);
+      line = bufferedReader.readLine();
+    }
+    bufferedReader.close();
+    sb.toString();
+  }
+
+  def contentIS: InputStream = {
+    if (_contentIS == null) null
+    else new ByteArrayInputStream(_contentIS.getBytes("UTF-8"))
+  }
+
 }
 
 object CMessage {
